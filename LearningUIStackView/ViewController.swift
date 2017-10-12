757ed2fd1caa8a2c5bd8b205f9e393655ebae8df
Rommel Rico <me@rommelrico.com>
2017-10-12 08:41:31 -0700

    Setting up the ViewController Segmented controls

--- LearningUIStackView/ViewController.swift
+++ LearningUIStackView/ViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var peopleStackView: UIStackView!
+    
+    @IBAction func distributionSelected(_ sender: UISegmentedControl) {
+        UIView.animate(withDuration: 1.0, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.2,options: .curveEaseInOut, animations: {
+            
+            if sender.selectedSegmentIndex == 0 {
+                self.peopleStackView.alignment = .fill
+            } else if sender.selectedSegmentIndex == 1 {
+                self.peopleStackView.alignment = .top
+            } else if sender.selectedSegmentIndex == 2 {
+                self.peopleStackView.alignment = .center
+            } else if sender.selectedSegmentIndex == 3 {
+                self.peopleStackView.alignment = .bottom
+            }
+            
+        }, completion: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func alignmentSelected(_ sender: UISegmentedControl) {
+        UIView.animate(withDuration: 1.0, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.2, options: .curveEaseInOut, animations: {
+            if sender.selectedSegmentIndex == 0 {
+                self.peopleStackView.distribution = .fill
+            } else if sender.selectedSegmentIndex == 1 {
+                self.peopleStackView.distribution = .fillEqually
+            } else if sender.selectedSegmentIndex == 2 {
+                self.peopleStackView.distribution = .fillProportionally
+            } else if sender.selectedSegmentIndex == 3 {
+                self.peopleStackView.distribution = .equalSpacing
+            } else if sender.selectedSegmentIndex == 4 {
+                self.peopleStackView.distribution = .equalCentering
+            }
+        }, completion: nil)
+        
     }
-
-
+    
 }
-
